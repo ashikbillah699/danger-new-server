4410af0b222998b2fa53ea386dc8ab4932adc676
Md Ashik Billah <ashikbillah699@gmail.com>
2022-10-22 09:29:43 +0600

    news json add

--- index.js
+++ index.js
@@ -6,12 +6,36 @@ const port = process.env.PORT || 5000
 app.use(cors())
 const categories = require('./data/categories.json')
 
+app.use(cors())
+const news = require('./data/news.json')
+
 app.get('/', (req, res) => {
     res.send('Helo World!')
 })
 
 app.get('/news-categories', (req, res) => {
     res.send(categories)
+})
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === '08') {
+        res.send(news)
+    }
+    else {
+        const categoryNews = news.filter(n => n.category_id === id)
+        res.send(categoryNews)
+    }
+})
+
+app.get('/news', (req, res) => {
+    res.send(news);
+})
+
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const seletedNews = news.find(n => n._id === id)
+    res.send(seletedNews);
 })
 
 app.listen(port, () => {
